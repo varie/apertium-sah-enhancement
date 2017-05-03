808b15256535cbfb712a5ef74c33c14c75651530
Roman Yangarber <roman.saku@gmail.com>
2017-05-03 20:16:15 +0300

    fix bugs in yaml, and wrong alphabet CC in lexicon

--- tests/run-all-yamls.sh
+++ tests/run-all-yamls.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-for yaml in *.yaml
+# *.yaml
+for yaml in \
+   aorist.yaml   \
+   cases.yaml  \
+   miscellaneous.yaml  \
+   necessitative.yaml  \
+   russian.yaml \
+   possessives.yaml  \
+   possessives_plural_wiki_sakhatyla.yaml  \
+   possessives_singular_wiki_sakhatyla.yaml  \
+   cases_plural.yaml  \
+   cases_wiki_sakhatyla.yaml
+
    do
-   echo "doing $yaml"
+   echo "============================================================ Doing $yaml."
    aq-morftest --colour --hide-passes --verbose $yaml
-   echo "Done $yaml."
+   echo "============================================================ Done  $yaml."
    done
